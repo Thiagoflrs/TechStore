5ef779f506c1bafac6ea775af6e6379a2f4b0a5f
Thiago Rodrigues <https://github.com/Thiagoflrs>
2026-03-13 13:06:33 -0300

    Add images - folders

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import Banner from "../../components/Banner/Banner";
 import Categories from "../../components/Categories/Categories";
 import ProductSection from "../../components/ProductSection/ProductSection";
 import SectionDivider from "../../components/SectionDivider/SectionDivider";
-import Banner1 from "../../assets/images/banner.webp";
-import Banner2 from "../../assets/images/banner1.webp";
-import Banner3 from "../../assets/images/banner2.webp";
+import Banner1 from "../../assets/images/banners/banner.webp";
+import Banner2 from "../../assets/images/banners/banner1.webp";
+import Banner3 from "../../assets/images/banners/banner2.webp";
 
 function Home() {
   return (
@@ -15,7 +15,7 @@ function Home() {
       <Categories title="Confira nossas Categorias" />
       <ProductSection title="Em oferta" />
       <SectionDivider images={[Banner1]} />
-      <ProductSection title="Mais vendidos" />
+      <ProductSection title="Mais Procurados" />
       <SectionDivider images={[Banner2, Banner3]} />
     </>
   );
