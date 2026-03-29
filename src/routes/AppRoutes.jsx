f43a7d8d338cbe395e772398b7109597f38f6a5b
Thiago Rodrigues <https://github.com/Thiagoflrs>
2026-03-29 20:33:51 -0300

    Changed AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "../pages/Auth/Auth";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import Home from "../pages/Home/Home"; 
+import { paths } from "./paths.js"
+import Home from "../pages/home/Home.jsx";
+import Auth from "../pages/Auth/Auth.jsx"
+import Payments from "../pages/Payments/Payments.jsx"
+import ProductDetails from "../pages/ProductDetails/ProductDetails.jsx"
+import Category from "../pages/Category/Category.jsx"
+import Dashboard from "../pages/Dashboard/Dashboard.jsx";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/register" element={<Auth />} />
-
-        {/* Rota do Dashboard liberada para teste */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path={paths.public.home} element={<Home />} />
+        <Route path={paths.public.auth} element={<Auth />} />
+        <Route path={paths.public.payments} element={<Payments />} />
+        <Route path={paths.public.productDetails} element={<ProductDetails />} />
+        <Route path={paths.public.categoryName} element={<Category />} />
+        <Route path={paths.private.dashboard} element={<Dashboard />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
